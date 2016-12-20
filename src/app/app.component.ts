3cab3045021f160593517157f5984ae6521b0253
aballinger1981 <andrew@andrewballinger.com>
2016-12-20 12:16:00 -0500

    added keyup event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Calculator!';
+
+  @HostListener('window:keyup', ['$event'])
+  test(event: any) {
+    console.log(event.key);
+  }
 }
