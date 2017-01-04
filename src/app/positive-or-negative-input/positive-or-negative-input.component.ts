b20ee93251b5f4fc21cd4efa878bb6d7059db2c6
aballinger1981 <andrew@andrewballinger.com>
2017-01-03 21:16:45 -0500

    +/- button changes current number to positive or negative value

--- src/app/positive-or-negative-input/positive-or-negative-input.component.ts
+++ src/app/positive-or-negative-input/positive-or-negative-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-positive-or-negative-input',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PositiveOrNegativeInputComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storageService: StorageService) { }
 
   ngOnInit() {
   }
 
+  input() {
+    if (!this.storageService.inputs.length || this.storageService.endOfInputs.operator) {
+      return;
+    }
+
+    const valueAsString = this.storageService.endOfInputs.value.toString();
+
+    if (valueAsString.charAt(0) === '-') {
+      this.storageService.endOfInputs.value = +valueAsString.slice(1);
+      console.log(this.storageService.endOfInputs.value);
+    } else {
+      const negative = '-';
+      this.storageService.endOfInputs.value = +negative.concat(valueAsString);
+      console.log(this.storageService.endOfInputs.value);
+    }
+  }
 }
